d540ec95f17cfbe8677fa878c46982be4cfe030a
Rein Van Leirsberghe <reinvanleirsberghe@hotmail.com>
2023-06-30 10:12:42 +0200

    screen orientation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MicroFrontendService } from '@liantis-ds/micro-frontend-tools';
+import { ScreenOrientation } from '@capacitor/screen-orientation';
 
 @Component({
   selector: 'portal-client1-root',
@@ -15,5 +16,21 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.microFrontendService.init();
+
+    ScreenOrientation.addListener(
+      'screenOrientationChange',
+      this.handleOrientationChange
+    );
+  }
+
+  handleOrientationChange() {
+    ScreenOrientation.orientation()
+      .then((result) => {
+        console.log('CLIENT1 - current orientation result', result);
+        // Perform any actions based on the new orientation
+      })
+      .catch((error) => {
+        console.error('CLIENT2 - Error getting orientation:', error);
+      });
   }
 }
